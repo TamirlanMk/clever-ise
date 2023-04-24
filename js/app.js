98ab31e97cbf8377c75d3778f98744167831e937
Tamirlan Mukashev <tamirlan_mukashev@mail.ru>
2023-04-25 04:02:44 +0600

    delete unused files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let linksWithScroll = document.querySelectorAll('.with-scroll');
 
+    linksWithScroll.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            let attr = link.getAttribute('href');
+            let section = document.querySelector(attr);
+
+            window.scrollTo({
+                behavior: "smooth",
+                top: section.offsetTop - 50
+            })
+
+        })
+    });
 });
